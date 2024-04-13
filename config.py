674ac2dfa12af4a66ff6f5dcd6ab159524be1460
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-04-13 17:33:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mr_editx")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/angel_world11")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tmm_heroku_world")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tmm_heroku_world")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
