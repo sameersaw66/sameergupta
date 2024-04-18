7e8a54fe28d2607cd55cabfc2d5bd8df99f28dfb
TMM-TEAM <150228624+TMM-TEAM@users.noreply.github.com>
2024-04-18 06:44:55 +0530

    Update userbot.py

--- AarohiX/core/userbot.py
+++ AarohiX/core/userbot.py
@@ -54,8 +54,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("mr_editx")
-                await self.one.join_chat("angel_world11")
+                await self.one.join_chat("tmm_support_chat")
+                await self.one.join_chat("tmm_heroku_world")
             except:
                 pass
             assistants.append(1)
@@ -83,8 +83,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("the_friendz")
-                await self.two.join_chat("roy_editx")
+                await self.two.join_chat("tmm_support_chat")
+                await self.two.join_chat("tmm_heroku_world")
             except:
                 pass
             assistants.append(2)
@@ -112,8 +112,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("angel_world11")
-                await self.three.join_chat("angel_world11")
+                await self.three.join_chat("tmm_support_chat")
+                await self.three.join_chat("tmm_support_chat")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("angel_world11")
-                await self.four.join_chat("angel_world11")
+                await self.four.join_chat("tmm_support_chat")
+                await self.four.join_chat("tmm_support_chat")
             except:
                 pass
             assistants.append(4)
@@ -170,8 +170,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("angel_world11")
-                await self.five.join_chat("angel_world11")
+                await self.five.join_chat("tmm_support_chat")
+                await self.five.join_chat("tmm_support_chat")
             except:
                 pass
             assistants.append(5)
