3904b1d667ca8f37f56d728e756522dd34fdfdda
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2024-06-02 10:45:55 +0530

    Update Welcome.py

--- AarohiX/plugins/Management/Welcome.py
+++ AarohiX/plugins/Management/Welcome.py
@@ -46,7 +46,7 @@ def circle(pfp, size=(500, 500)):
 
 
 def welcomepic(pic, user, chatname, id, uname):
-    background = Image.open("AarohiX/assets/dil.png")
+    background = Image.open("AarohiX/assets/mohmaya.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize((1157, 1158))
